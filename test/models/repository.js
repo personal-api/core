00993ec85e83627c86627be431f0403c93aa951c
Chris Vogt <mail@chrisvogt.me>
2018-08-13 00:05:16 -0700

    feat: add unit tests for project and repository getters (#2)

---
+++ test/models/repository.js
@@ -0,0 +1,13 @@
+import { expect } from 'chai';
+import { getLastUpdateDate } from '../../src/models/repository';
+
+const repositoryFixture = {
+  updated_at: new Date()
+};
+
+describe('model: repository getters', () => {
+  describe('getLastUpdateDate', () => {
+    const lastUpdated = getLastUpdateDate(repositoryFixture);
+    expect(lastUpdated).to.equal(repositoryFixture.updated_at);
+  });
+});
