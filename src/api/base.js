168dfef317782dd11e7e3e0d62496ebac8000c67
Chris Vogt <mail@chrisvogt.me>
2018-08-18 18:45:40 -0700

    fix: UnhandledPromiseRejectionWarning (#5)

--- src/api/base.js
+++ src/api/base.js
@@ -1,12 +1,12 @@
 export const error = (res, err) => {
   const message = err.toString();
 
+  res.type('json');
+  res.status = 500;
   res.json({
     status: 'error',
     message
   });
-  res.status = 500;
-  res.type('json');
 };
 
 export const success = (res, result) => {
@@ -18,7 +18,7 @@ export const success = (res, result) => {
     response.result = result;
   }
 
-  res.json(response);
-  res.status = 200;
   res.type('json');
+  res.status = 200;
+  res.json(response);
 };
