446bdc4a1ed9eb44458c0084ee4f40285fb174c1
Chris Vogt <mail@chrisvogt.me>
2018-08-20 22:46:51 -0700

    chore: add unit tests for the repositories controller

---
+++ test/controllers/repositories.js
@@ -0,0 +1,42 @@
+import { expect } from 'chai';
+import proxyquire from 'proxyquire';
+import sinon from 'sinon';
+
+import repositoryFixture from '../_fixtures/repository';
+
+const sandbox = sinon.createSandbox();
+const stubGetAllDocuments = sandbox.stub().resolves([repositoryFixture]);
+
+const {
+  getRepositories
+} = proxyquire
+  .noCallThru()
+  .load('../../src/controllers/repositories.js', {
+    '../database/services': {
+      getAllDocuments: stubGetAllDocuments
+    }
+});
+
+describe('repositories controller', () => {
+  afterEach(() => {
+    sandbox.resetHistory();
+  });
+
+  describe('getRepositories', () => {
+    const repositoriesArr = [repositoryFixture, repositoryFixture];
+    let allDocuments;
+
+    beforeEach(async () => {
+      stubGetAllDocuments.resolves(repositoriesArr);
+      allDocuments = await getRepositories();
+    });
+
+    it('queries the repositories collection', () => {
+      expect(stubGetAllDocuments.args).to.deep.equal([['repositories']]);
+    });
+
+    it('returns the repositories', () => {
+      expect(allDocuments).to.deep.equal(repositoriesArr);
+    });
+  });
+});
