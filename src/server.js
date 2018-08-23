446687961f374e08ce5815780b693b8cfb603cd9
Chris Vogt <mail@chrisvogt.me>
2018-08-22 23:47:35 -0700

    refactor: API routes into their own module

--- src/server.js
+++ src/server.js
@@ -1,9 +1,9 @@
 import express from 'express';
 
 import constants from './constants';
-import { getProjects, getProjectsWithRepos } from './api/projects';
-import { getRepositories } from './api/repositories';
-import middlewares from './middlewares';
+
+import applyMiddlewares from './middlewares';
+import applyRoutes from './routes';
 
 const {
   PORT,
@@ -13,11 +13,8 @@ const {
 const bootstrap = async () => {
   const app = express();
 
-  await middlewares(app);
-
-  app.get('/projects', getProjects);
-  app.get('/projectsWithRepos', getProjectsWithRepos);
-  app.get('/repositories', getRepositories);
+  applyMiddlewares(app);
+  applyRoutes(app);
 
   app.listen(PORT, () => console.log(SERVER_STARTED.replace('$PORT', PORT)));
 };
