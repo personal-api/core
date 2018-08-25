cef99c54ec5918b95772ba31495aa78d398e3afa
Chris Vogt <mail@chrisvogt.me>
2018-08-24 20:22:25 -0700

    feat: add response caching using apicache

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,13 @@
+import apicache from 'apicache';
+
 import { getProjects, getProjectsWithRepos } from './api/projects';
 import { getRepositories } from './api/repositories';
 
+const cacheOptions = { debug: true };
+const cache = apicache.options(cacheOptions).middleware;
+
 export default (app) => {
-  app.get('/projects', getProjects);
-  app.get('/projectsWithRepos', getProjectsWithRepos);
-  app.get('/repositories', getRepositories);
+  app.get('/projects', cache('3 hours'), getProjects);
+  app.get('/projectsWithRepos', cache('3 hours'), getProjectsWithRepos);
+  app.get('/repositories', cache('3 hours'), getRepositories);
 };
