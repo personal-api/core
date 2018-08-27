30d8029d3cb656f59f3731a0d73e21870194c12a
Chris Vogt <mail@chrisvogt.me>
2018-08-26 20:06:38 -0700

    feat: add and connect a composite route

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 import apicache from 'apicache';
 
 import constants from './constants';
+import getComposite from './controllers/composite';
 import getMetas from './controllers/metas';
 import getProfiles from './controllers/profiles';
 import getProjects from './controllers/projects';
@@ -15,6 +16,7 @@ const cache = apicache.options(cacheOptions).middleware;
 const { DEFAULT_CACHE_TIME } = constants;
 
 export default (app) => {
+  app.use('/composite', cache(DEFAULT_CACHE_TIME), getComposite)
   app.use('/metas', cache(DEFAULT_CACHE_TIME), getMetas);
   app.use('/profiles', cache(DEFAULT_CACHE_TIME), getProfiles);
   app.use('/projects', cache(DEFAULT_CACHE_TIME), getProjects);
