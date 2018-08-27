64d4855d55c09905d601944e6769ae81ca531df8
Chris Vogt <mail@chrisvogt.me>
2018-08-26 19:51:16 -0700

    feat: add routes to get profiles and metas

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,8 @@
 import apicache from 'apicache';
 
 import constants from './constants';
+import getMetas from './controllers/metas';
+import getProfiles from './controllers/profiles';
 import getProjects from './controllers/projects';
 import getRepositories from './controllers/repositories';
 
@@ -13,6 +15,8 @@ const cache = apicache.options(cacheOptions).middleware;
 const { DEFAULT_CACHE_TIME } = constants;
 
 export default (app) => {
+  app.use('/metas', cache(DEFAULT_CACHE_TIME), getMetas);
+  app.use('/profiles', cache(DEFAULT_CACHE_TIME), getProfiles);
   app.use('/projects', cache(DEFAULT_CACHE_TIME), getProjects);
   app.use('/repositories', cache(DEFAULT_CACHE_TIME), getRepositories);
 };
